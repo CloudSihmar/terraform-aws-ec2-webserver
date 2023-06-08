a13f5eaeb7417bf511a5ec806a25b372c99ce427
root <root@ip-172-31-5-178.ap-south-1.compute.internal>
2023-06-08 09:58:43 +0000

    updated

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "server" {
       sudo apt-get update -u
       sudo apt install -y nginx
       sudo systemctl start nginx
-      sudo echo "<html><body><h1>Hello this is module-1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      sudo echo "<html><body><h1>Hello this is terrafrom-registry-module-1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
       EOF
 
 tags = {
